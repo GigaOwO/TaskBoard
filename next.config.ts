f2133f780d3690dd67465c0db56b5a51fa4d08b4
GigaOwO <takumagiga1118@gmail.com>
2025-11-04 16:54:52 +0900

    クリックジャッキングを対策

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: "frame-ancestors 'none';",
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
